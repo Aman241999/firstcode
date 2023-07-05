1d6d12adebcd28d6c7189a816eca5f24271d676b
Aman241999 <amansinghania453>
2023-07-05 20:58:29 +0530

    aman

---
+++ dom.js
@@ -0,0 +1,5 @@
+let header=document.getElementById("main-header");
+header.style.borderBottom= "3px solid black";
+let title=document.getElementsByClassName("title");
+title[0].style.fontWeight="bold";
+title[0].style.color="green";
